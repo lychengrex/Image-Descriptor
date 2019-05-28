4d269684779e9caba40e0a622d61b9a0e565756b
Lin-Ying Cheng <lychengr3x@gmail.com>
2019-05-27 17:41:56 -0700

    Move build_vocab.py and resize.py to src/

--- src/build_vocab.py
+++ src/build_vocab.py
@@ -78,9 +78,9 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--caption_path', type=str,
-                        default='data/annotations/captions_train2014.json',
+                        default='../data/annotations/captions_train2014.json',
                         help='path for train annotation file')
-    parser.add_argument('--vocab_path', type=str, default='./data/vocab.pkl',
+    parser.add_argument('--vocab_path', type=str, default='../data/vocab.pkl',
                         help='path for saving vocabulary wrapper')
     parser.add_argument('--threshold', type=int, default=4,
                         help='minimum word count threshold')
